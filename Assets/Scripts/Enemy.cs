cb73b2cb75b00c4c4ce9f470b4dc57560fb6fd9d
BrendanVotano <brendan@bluevolcano.co>
2020-09-14 15:46:11 +1000

    Coroutines and Enemy Spawning

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Talk());
+        //StartCoroutine(MoveRandom());
+        StartCoroutine(MoveInDirection());
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+            StopAllCoroutines();
+    }
+
+    IEnumerator Talk()
+    {
+        print("Hello...");
+        yield return new WaitForSeconds(1);
+        print("My name is");
+        yield return new WaitForSeconds(1);
+        print("...");
+        yield return new WaitForSeconds(0.2f);
+        print("I forget");
+    }
+
+    IEnumerator MoveRandom()
+    {
+        Vector3 newPos = new Vector3(Random.Range(-5f,5f), 0, Random.Range(-5f, 5f));
+        transform.position = newPos;
+        yield return new WaitForSeconds(3);
+        StartCoroutine(MoveRandom());
+    }
+
+    IEnumerator MoveInDirection()
+    {
+        for (int i = 0; i < 500; i++)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime);
+            yield return null;
+        }
+        transform.Rotate(Vector3.up * 180);
+        yield return new WaitForSeconds(1);
+        StartCoroutine(MoveInDirection());        
+    }
+}
---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public int spawnCount = 5;
+    public float spawnDelay = 1;
+    int enemyCount = 0;
+    public GameObject[] enemyPrefabs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnWithDelay(spawnDelay));
+    }
+
+    IEnumerator SpawnWithDelay(float _delay)
+    {
+        while (enemyCount < spawnCount)
+        {
+            int rnd = Random.Range(0, enemyPrefabs.Length);
+            Vector3 newPos = new Vector3(Random.Range(-5f, 5f), 0, Random.Range(-5f, 5f));
+            GameObject go = Instantiate(enemyPrefabs[rnd], newPos, transform.rotation);
+            enemyCount++;
+            yield return new WaitForSeconds(_delay);
+        }
+    }
+}
